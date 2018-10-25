3ca5f288532d24f95a05ca1d96f47c163e9a3594
ajithvgiri <mail.ajithvgiri@gmail.com>
2018-10-25 21:35:30 +0530

    Initial commit

---
+++ app/src/main/java/com/ajithvgiri/apptemplate/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.ajithvgiri.apptemplate
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.ajithvgiri.apptemplate.activity.FABActivity
+import com.ajithvgiri.apptemplate.fragment.GalleryFragment
+import com.ajithvgiri.apptemplate.fragment.ImportFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            //uncomment this line if you want to show snackbar
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+
+            //To start a new Activity
+            startActivity(Intent(this, FABActivity::class.java))
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action (passing Import fragment to fragmentCommit method)
+                fragmentCommit(ImportFragment())
+            }
+            R.id.nav_gallery -> {
+                // passing Gallery fragment to fragmentCommit method)
+                fragmentCommit(GalleryFragment())
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun fragmentCommit(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        // replace the fragment
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        //commit the fragment in frameLayout which is found in content_main.xml
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/ajithvgiri/apptemplate/activity/FABActivity.kt
@@ -0,0 +1,13 @@
+package com.ajithvgiri.apptemplate.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ajithvgiri.apptemplate.R
+
+class FABActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fab)
+    }
+}
